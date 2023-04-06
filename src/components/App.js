d54de8e71aa32321ec0e07bda633c5a194411b76
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-04-06 23:14:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,32 @@
-
-
-  
-  
- import React, { useState, useEffect } from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
 const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState(true);
-const API_KEY="e41a347e6921411e459893aafef98318";
+  const [loading, setLoading] = useState(false);
 
- const changeFunction=(e)=>
- {
-  setCategory(e.target.value);
- }
- useEffect(()=>
- {
-   setLoading(true);
-   fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=us&max=10&apikey=${API_KEY}`).then(res=>res.json())
-   .then((res)=>{
-    setNewsData(res.articles)
-    console.log(res);
-  }).then(()=>setLoading(false));
- },[category])
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=893f98f6bb8a5a60d38404afa9f0dadc&max=10&lang=en`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setNewsData(data.articles);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [category]);
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
   return (
     <div id="main">
-      <h1 className='heading'>Top 10 {category} news.</h1>
-      <select value={category} onChange={changeFunction}>
+      <h1 className="heading">Top 10 {category} news</h1>
+      <select value={category} onChange={handleCategoryChange}>
         <option value="general">General</option>
         <option value="business">Business</option>
         <option value="sports">Sports</option>
@@ -36,26 +35,27 @@ const API_KEY="e41a347e6921411e459893aafef98318";
         <option value="entertainment">Entertainment</option>
         <option value="science">Science</option>
       </select>
-      {loading && <p className='loader'>Loading...</p>}
-      {!loading &&<ol>
-       { newsData.map((e,i)=>
-        {
-          return (<li key={i}>
-          <img className='news-img' src={e.image} alt=""/>
-          <section className='new-title-content-author'>
-            <h3 className='news-title'>{e.title}</h3>
-            <section className='new-content-author'>
-              <p className='news-description'>{e.description}</p>
-              <p className='news-source'><strong>Source:</strong>{e.source.name}</p>
-            </section>
-          </section>
-        </li>)
-        })
-      }
-      </ol>}
+      {loading && <p className="loader">Loading...</p>}
+      {!loading && (
+        <ol>\
+          {newsData.map((news, index) => (
+            <li key={index}>
+              <img className="news-img" src={news.image} alt="" />
+              <section className="new-title-content-author">
+                <h3 className="news-title">{news.title}</h3>
+                <section className="new-content-author">
+                  <p className="news-description">{news.description}</p>
+                  <p className="news-source">
+                    <strong>Source:</strong> {news.source.name}
+                  </p>
+                </section>
+              </section>
+            </li>
+          ))}
+        </ol>
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
